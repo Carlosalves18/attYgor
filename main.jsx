94b6849e0bd0c5ed745e929d532d07f6bd4cbc15
Carlos Eduardo <138811560+Carlosalves18@users.noreply.github.com>
2024-03-25 18:19:53 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Footer from './footer/footer'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+   <Footer/>
+  </React.StrictMode>,
+)
